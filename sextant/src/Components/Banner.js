624e7e2e2385509a43f4dfe3f35c5d9ef1bbae81
Osy <40663679+therealosy@users.noreply.github.com>
2023-04-18 15:11:20 +0100

    Addded Banner Component

---
+++ sextant/src/Components/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Styles/Banner.css";
+
+export default function Banner() {
+  return (
+    <header className="banner">
+      <nav className="banner-nav">
+        <div className="banner-text">Sextant</div>
+      </nav>
+    </header>
+  );
+}
