a5f90446dab04bd0ef0e9e86beb865a787e2cca9
Osy <40663679+therealosy@users.noreply.github.com>
2023-04-19 12:36:14 +0100

    Modified component to meet new requirements

--- sextant/src/Components/Exhibit.js
+++ sextant/src/Components/Exhibit.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Card from "./Card";
 
 import "../Styles/Exhibit.css";
 
 export default function Exhibit(props) {
-  const metrics = [{ title: "IP Address", content: "127.0.0.1" }];
-
-  let mappedCard = metrics.map(({ title, content }) => (
-    <Card title={title} content={content} />
-  ));
-
-  return <div className="exhibit">{mappedCard}</div>;
+  return (
+    <div className="exhibit">
+      <div className="exhibit-title-container">
+        <h3 className="exhibit-title">{props.title}</h3>
+      </div>
+      <div className="exhibit-content">{props.children}</div>
+    </div>
+  );
 }
