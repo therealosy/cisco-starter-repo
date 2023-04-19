8c99b7f2755328039a365ee3d542cf7d06221a56
Osy <40663679+therealosy@users.noreply.github.com>
2023-04-19 12:36:38 +0100

    Imported IP address component

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import Banner from "./Components/Banner";
 import Exhibit from "./Components/Exhibit";
+import IPAddressComponent from "./Components/IPAddressComponent";
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <Exhibit />
+      <div className="exhibit-container">
+        <Exhibit title={"IPv4 Address"}>
+          <IPAddressComponent ipVersion={"IPv4"} />
+        </Exhibit>
+        <Exhibit title={"IPv6 Address"}>
+          <IPAddressComponent ipVersion={"IPv6"} />
+        </Exhibit>
+      </div>
     </div>
   );
 }
