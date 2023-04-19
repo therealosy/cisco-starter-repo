8e7d8cf319628e0b737884b1aa03fdbab2c66e94
Osy <40663679+therealosy@users.noreply.github.com>
2023-04-19 17:33:08 +0100

    Imported Latency component

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,17 +2,21 @@ import "./App.css";
 import Banner from "./Components/Banner";
 import Exhibit from "./Components/Exhibit";
 import IPAddressComponent from "./Components/IPAddressComponent";
+import PylonLatencyComponent from "./Components/PylonLatencyComponent";
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <div className="exhibit-container">
-        <Exhibit title={"IPv4 Address"}>
+        <Exhibit title={"Public IPv4 Address"}>
           <IPAddressComponent ipVersion={"IPv4"} />
         </Exhibit>
-        <Exhibit title={"IPv6 Address"}>
+        <Exhibit title={"Public IPv6 Address"}>
           <IPAddressComponent ipVersion={"IPv6"} />
+        </Exhibit>
+        <Exhibit title={"Pylon Latency"}>
+          <PylonLatencyComponent />
         </Exhibit>
       </div>
     </div>
